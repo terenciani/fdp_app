2395f07b4b2f3cf279873fdd200c9281eb85a041
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2015-10-20 22:05:07 -0300

    editText and Button

--- app/src/main/java/br/com/fabricadeprogramador/fdpapp/MainActivity.java
+++ app/src/main/java/br/com/fabricadeprogramador/fdpapp/MainActivity.java
@@ -9,11 +9,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        Button bt =  (Button) findViewById(R.id.btOk);
+        bt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText ed1 = (EditText) findViewById(R.id.ed1);
+                Toast.makeText(MainActivity.this ,"Oi " + ed1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
